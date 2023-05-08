df0e6c3e277282f2aaa7e8ec5834c499a1ad62ea
Austin <austinwu06@gmail.com>
2023-05-07 22:38:21 -0700

    Update cf_1a.py

--- codeforces/cf_1a.py
+++ codeforces/cf_1a.py
@@ -1,4 +1,17 @@
-import math
-n, m, a = map(int, input().split())
+# import math
+# n, m, a = map(int, input().split())
 
-print(math.ceil(n/a) + math.ceil(m/a))  
+# print(math.ceil(n/a) + math.ceil(m/a))  
+
+n,m,a = map(int, input().split)
+if m%a ==0:
+    v = m //a
+else:
+    v = m // a + 1
+
+if n%a ==0:
+    v2 = n //a
+else:
+    v2 = n //a + 1
+
+print(v * v2)
